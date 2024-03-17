162778fffb5181c8bf73a540247ef1ab8f35e8cf
Niranjan <2100090187@kluniversity.in>
2024-03-17 15:54:25 +0530

    login validation

--- AuthSystem/views.py
+++ AuthSystem/views.py
@@ -116,6 +116,9 @@ def loginUser(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
+        
+        # Validate password
+        
         if user is not None:
             login(request, user)
             return redirect("home")
